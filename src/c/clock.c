fc86545eb646396b5d35351d39291ebbd07d6949
GreenHex <GreenHex@gmail.com>
2016-11-26 18:42:29 +0530

    Added minute hand, which doesn't work
    - Now to convert it into a proper app, with start/ stop and reset controls.

--- src/c/clock.c
+++ src/c/clock.c
@@ -5,6 +5,8 @@
 #include <pebble.h>
 #include "clock.h"
 
+// #define DEBUG
+
 static Layer *window_layer = 0;
 static Layer *outline_layer = 0;
 static Layer *seconds_dial_layer = 0;
@@ -12,7 +14,21 @@ static Layer *minutes_dial_layer = 0;
 static Layer *seconds_layer = 0;
 static Layer *minutes_layer = 0;
 
-tm tm_time;
+static uint32_t seconds = 0;
+static uint32_t minutes = 0;
+static tm tm_time;
+
+static void print_pt( char *str, GPoint pt ) {
+  #ifdef DEBUG
+  APP_LOG( APP_LOG_LEVEL_INFO, "%s: ( %d, %d )", str, pt.x, pt.y );
+  #endif
+}
+
+static void print_rect( char *str, GRect rect ) {
+  #ifdef DEBUG
+  APP_LOG( APP_LOG_LEVEL_INFO, "%s: ( %d, %d, %d, %d )", str, rect.origin.x, rect.origin.y, rect.size.w, rect.size.h );
+  #endif
+}
 
 static void second_hand_uint32_setter( void *subject, uint32_t angle ) {
   ( ( HAND_LAYER_DATA *) layer_get_data( (Layer *) subject ) )->current_angle = angle;
@@ -44,13 +60,22 @@ static void start_seconds_animation( void ) {
   Animation *seconds_hand_animation = property_animation_get_animation( seconds_hand_prop_animation );
   animation_set_curve( seconds_hand_animation, AnimationCurveLinear );
   animation_set_delay( seconds_hand_animation, 0 );
-  animation_set_duration( seconds_hand_animation, 900 );
-  animation_schedule( seconds_hand_animation );
-  // animation_destroy( seconds_hand_animation );
+  animation_set_duration( seconds_hand_animation, 1000 );
+  
+  Animation **seconds_animation_array = (Animation**) malloc( ( NUM_ANIMATIONS ) * sizeof( Animation* ) );
+  seconds_animation_array[0] = seconds_hand_animation;
+  
+  Animation *spawn = animation_spawn_create_from_array( seconds_animation_array, NUM_ANIMATIONS );
+  animation_set_play_count( spawn, 1 );
+  animation_schedule( spawn );
+  free( seconds_animation_array );
 }
 
 static void handle_clock_tick( struct tm *tick_time, TimeUnits units_changed ) {
-  tm_time = *tick_time; // copy to global  
+  tm_time = *tick_time; // copy to global, just for fun, as it is not used
+  if ( units_changed & SECOND_UNIT ) ++seconds;
+  if ( units_changed & MINUTE_UNIT ) ++minutes;
+  
   #ifdef DEBUG
   APP_LOG( APP_LOG_LEVEL_INFO, "clock.c: handle_clock_tick(): %d:%02d:%02d", tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec );
   #endif
@@ -101,26 +126,36 @@ static void seconds_dial_update_proc( Layer *layer, GContext *ctx ) {
 static void minutes_dial_update_proc( Layer *layer, GContext *ctx ) { 
   GRect layer_bounds = layer_get_bounds( layer );
   graphics_context_set_antialiased( ctx, true );
-  graphics_context_set_fill_color( ctx, GColorYellow );
+  graphics_context_set_fill_color( ctx, GColorWhite );
   graphics_fill_rect( ctx, layer_bounds, layer_bounds.size.w / 2, GCornersAll );
   graphics_context_set_stroke_color( ctx, GColorLightGray );
   graphics_context_set_stroke_width( ctx, 1 );
   graphics_draw_circle( ctx, GPoint( layer_bounds.size.w / 2, layer_bounds.size.h / 2 ), layer_bounds.size.w / 2 - 1 );
 }
 
-static void minutes_hand_layer_update_proc ( Layer *layer, GContext *ctx ) {
+static void minutes_hand_layer_update_proc( Layer *layer, GContext *ctx ) {
   GRect layer_bounds = layer_get_bounds( layer );
+  
   HAND_LAYER_DATA *minutes_layer_data = (HAND_LAYER_DATA *) layer_get_data( minutes_layer );
   
+  uint32_t min_angle = TRIG_MAX_ANGLE * ( minutes * 4 / 60 ) ;
+  uint32_t min_tail_angle = min_angle + ( TRIG_MAX_ANGLE / 2 );
   GPoint center_pt = grect_center_point( &layer_bounds );
-  GPoint end_pt;
-  GPoint tail_end_pt;
+
+  GPoint min_end_point = (GPoint) {
+    .x = ( sin_lookup( min_angle ) * MINUTES_HAND_LENGTH / TRIG_MAX_RATIO ) + center_pt.x,
+    .y = ( -cos_lookup( min_angle ) * MINUTES_HAND_LENGTH / TRIG_MAX_RATIO ) + center_pt.y
+  };
+  GPoint min_tail_end_pt = (GPoint) {
+    .x = ( sin_lookup( min_tail_angle ) * MINUTES_HAND_TAIL_LENGTH / TRIG_MAX_RATIO ) + center_pt.x,
+    .y = ( -cos_lookup( min_tail_angle ) * MINUTES_HAND_TAIL_LENGTH / TRIG_MAX_RATIO ) + center_pt.y
+  };
   
   HAND_DRAW_PARAMS hand_draw_params = (HAND_DRAW_PARAMS) {
     .ctx = ctx,
     .center_pt = center_pt,
-    .from_pt = center_pt,
-    .to_pt = center_pt,
+    .from_pt = min_end_point,
+    .to_pt = min_tail_end_pt,
     .hand_width = minutes_layer_data->width,
     .hand_color = GColorFromHEX( minutes_layer_data->colour ),
     .hand_outline_color = GColorWhite,
@@ -137,17 +172,19 @@ static void seconds_hand_layer_update_proc( Layer *layer, GContext *ctx ) {
   HAND_LAYER_DATA *seconds_layer_data = (HAND_LAYER_DATA *) layer_get_data( seconds_layer );
   
   GPoint center_pt = grect_center_point( &layer_bounds );
-  GPoint end_pt;
-  GPoint tail_end_pt;
   
-  end_pt.x = ( sin_lookup( seconds_layer_data->current_angle % TRIG_MAX_ANGLE ) *
-              seconds_layer_data->length / TRIG_MAX_RATIO ) + center_pt.x;
-  end_pt.y = ( -cos_lookup( seconds_layer_data->current_angle % TRIG_MAX_ANGLE ) *
-              seconds_layer_data->length / TRIG_MAX_RATIO ) + center_pt.y;
-  tail_end_pt.x = ( sin_lookup( ( ( seconds_layer_data->current_angle ) - TRIG_MAX_ANGLE / 2 ) % TRIG_MAX_ANGLE ) * 
-                   seconds_layer_data->tail_length / TRIG_MAX_RATIO ) + center_pt.x;
-  tail_end_pt.y = ( -cos_lookup( ( ( seconds_layer_data->current_angle ) - TRIG_MAX_ANGLE / 2 ) % TRIG_MAX_ANGLE ) *
-                   seconds_layer_data->tail_length / TRIG_MAX_RATIO ) + center_pt.y;
+  GPoint end_pt = (GPoint) {
+    .x = ( sin_lookup( seconds_layer_data->current_angle % TRIG_MAX_ANGLE ) *
+              seconds_layer_data->length / TRIG_MAX_RATIO ) + center_pt.x,
+    .y = ( -cos_lookup( seconds_layer_data->current_angle % TRIG_MAX_ANGLE ) *
+              seconds_layer_data->length / TRIG_MAX_RATIO ) + center_pt.y
+  };
+  GPoint tail_end_pt = (GPoint) {
+    .x = ( sin_lookup( ( ( seconds_layer_data->current_angle ) - TRIG_MAX_ANGLE / 2 ) % TRIG_MAX_ANGLE ) * 
+                   seconds_layer_data->tail_length / TRIG_MAX_RATIO ) + center_pt.x,
+    .y = ( -cos_lookup( ( ( seconds_layer_data->current_angle ) - TRIG_MAX_ANGLE / 2 ) % TRIG_MAX_ANGLE ) *
+                   seconds_layer_data->tail_length / TRIG_MAX_RATIO ) + center_pt.y
+  };
   
   HAND_DRAW_PARAMS hand_draw_params = (HAND_DRAW_PARAMS) {
     .ctx = ctx,
@@ -181,7 +218,7 @@ void clock_init( Window *window ){
 
   minutes_dial_layer = layer_create( MINUTES_RECT_FRAME );
   layer_set_update_proc( minutes_dial_layer, minutes_dial_update_proc );
-  layer_add_child( seconds_dial_layer, minutes_dial_layer );
+  layer_add_child( outline_layer, minutes_dial_layer );
   
   minutes_layer = layer_create_with_data( MINUTES_RECT_FRAME, sizeof( HAND_LAYER_DATA ) );
   minutes_layer_data = (HAND_LAYER_DATA *) layer_get_data( minutes_layer ); 
@@ -191,8 +228,8 @@ void clock_init( Window *window ){
   minutes_layer_data->width = MINUTES_HAND_THK;
   minutes_layer_data->bounds = MINUTES_RECT_FRAME;
   minutes_layer_data->hub_radius = MINUTES_HUB_RADIUS;
-  minutes_layer_data->current_angle = tm_time.tm_min;
-  minutes_layer_data->next_angle = tm_time.tm_min;
+  minutes_layer_data->current_angle = 0;
+  minutes_layer_data->next_angle = 0;
   layer_set_update_proc( minutes_layer, minutes_hand_layer_update_proc );
   layer_add_child( outline_layer, minutes_layer );
 
@@ -204,8 +241,8 @@ void clock_init( Window *window ){
   seconds_layer_data->width = SECONDS_HAND_THK;
   seconds_layer_data->bounds = SECONDS_RECT_FRAME;
   seconds_layer_data->hub_radius = SECONDS_HUB_RADIUS;
-  seconds_layer_data->current_angle = tm_time.tm_sec;
-  seconds_layer_data->next_angle = tm_time.tm_sec;
+  seconds_layer_data->current_angle = 0;
+  seconds_layer_data->next_angle = 0;
   layer_set_update_proc( seconds_layer, seconds_hand_layer_update_proc );
   layer_add_child( outline_layer, seconds_layer );
   
